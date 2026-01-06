84e47c5e3584881f1b15d8cd7b687835bc81a33d
Diogo Patino <diogop3@nycstudents.net>
2026-01-06 18:04:49 +0000

    3.3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function Image1(){
+    document.getElementById("highlight").innerHTML="Image 1";
+}
+
+function Image2(){
+    document.getElementById("highlight").innerHTML="Image 2";
+}
+
+function Image3(){
+    document.getElementById("highlight").innerHTML="Image 3";
+}
+
+function Image4(){
+    document.getElementById("highlight").innerHTML = "Image 4";
+}
+
+function Image5(){
+    document.getElementById("highlight").innerHTML = "Image 5";
+}
